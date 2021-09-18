78762cec4c86971f2a56474fd4deeb87ca8ab76e
Dan Bruce <dan@ddbruce.com>
2021-09-18 12:43:05 -0400

    add stuff for debugging

--- src/server.js
+++ src/server.js
@@ -5,6 +5,17 @@ require("dotenv").config();
 const { app } = require("./utilities/bolt.js");
 const { processMessage } = require("./utilities/helperFunctions.js");
 
+//globals
+let DEV;
+if (process.env.ENVIRONMENT && process.env.ENVIRONMENT == "dev") {
+  DEV = true;
+} else {
+  DEV = false;
+}
+
 app.message(async ({ message }) => {
+  if (DEV) {
+    console.log(message);
+  }
   await processMessage(message);
 });
